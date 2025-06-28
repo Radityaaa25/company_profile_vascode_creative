002e9dd225b9d533b66100ea9bb77360daf338a6
Raditya <radityaanandasatria@gmail.com>
2025-06-28 23:44:39 +0700

    commit awal

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-center text-red-500 font-bold'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
