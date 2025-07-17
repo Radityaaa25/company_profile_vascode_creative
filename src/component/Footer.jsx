af47fcf38732e93ed1947353e226d56c69af567a
Raditya <radityaanandasatria@gmail.com>
2025-07-17 15:23:28 +0700

    Tamplate Awal Selesai

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,6 +1,29 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer bg-white shadow mt-20" id="social">
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        <p className="py-4">
+          &copy; Copyright by <span className="font-bold">Vascode Creative</span>
+        </p>
+        <div className="social-footer flex items-center sm:gap-7 gap-2">
+          <a href="#">
+            <i className="ri-facebook-circle-fill text-2xl"></i>
+          </a>
+          <a href="#">
+            <i className="ri-twitter-fill text-2xl"></i>
+          </a>
+          <a href="#">
+            <i className="ri-youtube-fill text-2xl"></i>
+          </a>
+          <a href="#">
+            <i className="ri-linkedin-box-fill text-2xl"></i>
+          </a>
+          <a href="#">
+            <i className="ri-reddit-fill text-2xl"></i>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
